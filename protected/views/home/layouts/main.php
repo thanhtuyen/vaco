2fa1103a06c90761eb89a6ba511252600a371759
tamem1989 <tamem20091989@gmail.com>
2014-03-21 22:20:34 +0700

    change folder images in css file

--- protected/views/home/layouts/main.php
+++ protected/views/home/layouts/main.php
@@ -5,7 +5,7 @@
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="language" content="en" />
 
-  <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />
+  <!--<link rel="stylesheet" type="text/css" href="<?php //echo Yii::app()->theme->baseUrl; ?>/css/styles.css" />-->
 
   <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/common.css" />
 
